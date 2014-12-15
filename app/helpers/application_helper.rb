7992391014b413ffa41b2ee5054da2ebb0264eb7
Jeff Chiu <jeff@codefork.com>
2014-12-15 10:31:13 -0500

    created search_by_facet_value helper and use everywhere; cleanup url TODOs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     SDBMSS::Util.format_fuzzy_date(d)
   end
 
+  # returns a URL for a new search on a given facet and value
+  def search_by_facet_value facet_name, value
+    search_action_path(add_facet_params(facet_name, value, {}))
+  end
+
 end
