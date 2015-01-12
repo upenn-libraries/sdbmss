9ebb93d54062815b3ff93af312603bac4a5e7704
Jeff Chiu <jeff@codefork.com>
2015-01-12 13:39:27 -0500

    expanded list of materials, per this morning's meeting

--- app/models/entry_material.rb
+++ app/models/entry_material.rb
@@ -3,13 +3,18 @@ class EntryMaterial < ActiveRecord::Base
 
   MATERIAL_TYPES = [
     ["Bamboo", "Bamboo"],
+    ["Bark", "Bark"],
     ["Clay", "Clay"],
+    ["Leaf", "Leaf"],
     ["Leather", "Leather"],
+    ["Mica", "Mica"],
     ["Palm leaf", "Palm leaf"],
     ["Paper", "Paper"],
     ["Papyrus", "Papyrus"],
     ["Parchment", "Parchment"],
     ["Silk", "Silk"],
+    ["Skin", "Skin"],
+    ["Wax", "Wax"],
     ["Wood", "Wood"],
     ["Other", "Other"],
   ]
