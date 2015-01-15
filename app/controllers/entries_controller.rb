9b0430e412d3553a096887a7e95b9be2a80b92c4
Jeff Chiu <jeff@codefork.com>
2015-01-15 16:49:23 -0500

    implement delete functionality

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -95,8 +95,16 @@ class EntriesController < ApplicationController
   # we don't ever destroy anything, we just mark it as deleted
   def destroy
     @entry.deleted = true
+    @entry.updated_by_id = current_user.id
     @entry.save!
-    respond_with(@computer)
+    # if we call respond_with(@entry), which is more rails-ish, the
+    # response is a 302 to a #show, but jquery's ajax code gets stuck
+    # in an redirect loop, deleting the object over and over again. So
+    # we force-return a 200 with an empty body for JSON calls to this
+    # action.
+    respond_to do |format|
+      format.json { render :json => {}, :status => :ok }
+    end
   end
 
   private
