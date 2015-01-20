0117ae1a64a60a4f319fa3f87baf5c4fd31c3746
Jeff Chiu <jeff@codefork.com>
2015-01-20 09:38:21 -0500

    csv is already a registered mime type by default

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,8 +3,6 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 
-Mime::Type.register "text/csv", :csv
-
 ActionController::Renderers.add :csv do |obj, options|
   self.headers['Content-Disposition'] = "attachment; filename=#{options[:filename]}"
   self.headers['Content-Type'] = 'text/csv'
