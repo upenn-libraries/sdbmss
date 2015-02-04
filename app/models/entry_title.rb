9dae9f1c396c99e50d240f0728bac821fb3534a4
Jeff Chiu <jeff@codefork.com>
2015-02-04 12:20:23 -0500

    tweaks to form labels and display

--- app/models/entry_title.rb
+++ app/models/entry_title.rb
@@ -5,7 +5,7 @@ class EntryTitle < ActiveRecord::Base
   belongs_to :entry
 
   def to_s
-    (title || "") + (common_title ? "[#{common_title}]" : "") + certainty_flags
+    (title || "") + (common_title ? " [#{common_title}]" : "") + certainty_flags
   end
 
 end
