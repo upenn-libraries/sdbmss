2e415f42ce4d1e589eba0190e16adeb16f927b93
Jeff Chiu <jeff@codefork.com>
2015-03-04 22:29:33 -0500

    forgot to change env var name to SOLR_URL in script

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 # Wrapper script around rspec that does some prep so tests run fresh
 
 export RAILS_ENV=test
-export SDBMSS_SOLR_URL="http://127.0.0.1:8983/solr/test"
+export SOLR_URL="http://127.0.0.1:8983/solr/test"
 
 bundle exec rake tmp:clear db:drop db:create db:schema:load
 
