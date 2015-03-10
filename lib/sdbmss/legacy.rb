8724bcc10b1fe573c8e4a0ed8cd43ea0da38eed2
Jeff Chiu <jeff@codefork.com>
2015-03-10 16:45:22 -0400

    fix bug in current_location migration

--- lib/sdbmss/legacy.rb
+++ lib/sdbmss/legacy.rb
@@ -406,12 +406,14 @@ module SDBMSS::Legacy
         # if there's a Manuscript, tack the current_location info to
         # it. Otherwise, tack the info on to 'other_info' field.
         if manuscript.present?
-          if manuscript.current_location != row['CURRENT_LOCATION']
-            if manuscript.current_location.present?
-              create_issue('MANUSCRIPT', row['MANUSCRIPT_ID'], "current_location", "More than one current_location found for Manuscript ID = #{manuscript.id}")
-            else
-              manuscript.current_location = ""
-            end
+          if !manuscript.current_location.present?
+            manuscript.current_location = ""
+          else
+            create_issue('MANUSCRIPT', row['MANUSCRIPT_ID'], "current_location", "More than one current_location found for Manuscript ID = #{manuscript.id}")
+          end
+
+          # only append str if it's not already included in the field
+          if !manuscript.current_location.include?(row['CURRENT_LOCATION'])
             manuscript.current_location += "; " if manuscript.current_location.length > 0
             manuscript.current_location << row['CURRENT_LOCATION']
             manuscript.save!
