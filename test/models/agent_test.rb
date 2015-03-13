235e4734b8e69701c59399fb0327891b37d2d81e
Jeff Chiu <jeff@codefork.com>
2015-03-13 12:18:02 -0400

    add dashboard test; rm rails autogenerated tests since we use rspec

--- test/models/agent_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AgentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/entry_author_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class EntryAuthorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
