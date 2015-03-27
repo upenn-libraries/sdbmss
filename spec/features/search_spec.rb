9bcca1a8ad7e05dfb783d79e1639dff06ac45674
Jeff Chiu <jeff@codefork.com>
2015-03-27 16:02:39 -0400

    fix broken test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -77,7 +77,7 @@ describe "Blacklight Search", :js => true do
     visit advanced_search_path
 
     # all text search fields should show up in dropdown
-    expect(find_by_id('text_field_0').all("option").length).to eq(21)
+    expect(find_by_id('text_field_0').all("option").length).to eq(22)
     # all numeric search fields should show up in dropdown
     expect(find_by_id('numeric_field_0').all("option").length).to eq(12)
   end
