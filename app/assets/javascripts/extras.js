77d5c7d6d6e7d5275df19c32422a56fef21144bf
Jeff Chiu <jeff@codefork.com>
2015-05-13 15:10:32 -0400

    upgrade datatables

--- app/assets/javascripts/extras.js
+++ app/assets/javascripts/extras.js
@@ -6,7 +6,7 @@
 //= require angular-cookies.1.2.26
 //= require angular-resource.1.3.0
 //= require ui-bootstrap-tpls-0.12.1
-//= require jquery.dataTables
+//= require jquery.dataTables.min
 //= require dataTables.bootstrap
 //= require dataTables.colVis
 //= require dataTables.colReorder
