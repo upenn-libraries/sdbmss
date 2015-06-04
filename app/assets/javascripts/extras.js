02398b970c62e04385937ebb1803fdf195983b26
Jeff Chiu <jeff@codefork.com>
2015-06-04 17:01:37 -0400

    use minified js libraries

--- app/assets/javascripts/extras.js
+++ app/assets/javascripts/extras.js
@@ -1,11 +1,11 @@
 // For pages (mostly post-login) with a lot of js functionality
 // This assumes application.js has been included.
 
-//= require jquery.qtip
+//= require jquery.qtip.min
 //= require angular.1.3.15
 //= require angular-cookies.1.2.26
-//= require angular-resource.1.3.0
-//= require ui-bootstrap-tpls-0.12.1
+//= require angular-resource.1.3.0.min
+//= require ui-bootstrap-tpls-0.12.1.min
 //= require jquery.dataTables.min
 //= require dataTables.bootstrap
 //= require dataTables.colVis
