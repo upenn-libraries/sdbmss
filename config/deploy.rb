7e463d3e6b4580222d6da2128a4247847d1ee398
Jeff Chiu <jeff@codefork.com>
2015-06-19 22:08:31 -0400

    use shared dir for log

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :deploy_to, '/var/www/sdbmss/'
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
