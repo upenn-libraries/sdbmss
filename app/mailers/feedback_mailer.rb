bd091ee9a8d043ae3784379ac8fa0014f0ebf15d
Jeff Chiu <jeff@codefork.com>
2015-07-01 16:01:40 -0400

    better subject

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -4,7 +4,7 @@ class FeedbackMailer < ActionMailer::Base
   def feedback_email(from, subject, body)
     @body = body
     to = "jeffchiu@upenn.edu" # ENV.fetch('SDBMSS_EMAIL_FROM')
-    mail(from: from, to: to , subject: subject)
+    mail(from: from, to: to , subject: "Feedback from New SDBM Website: #{subject}")
   end
 
 end
