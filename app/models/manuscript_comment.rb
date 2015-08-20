2a95b0e4f56b3751f427f1cb5fea025c82cab429
Jeff Chiu <jeff@codefork.com>
2015-08-19 22:05:12 -0400

    add back associations, taking them out broke the migration script

--- app/models/manuscript_comment.rb
+++ app/models/manuscript_comment.rb
@@ -5,5 +5,7 @@ class ManuscriptComment < ActiveRecord::Base
   validates_presence_of :manuscript
   validates_presence_of :comment
 
+  accepts_nested_attributes_for :comment, allow_destroy: true
+
   include HasPaperTrail
 end
