71155ef3ca186ca9e4b6164017c191b1be4a6a9d
Jeff Chiu <jeff@codefork.com>
2015-09-15 16:35:54 -0400

    add comment

--- app/models/entry_change.rb
+++ app/models/entry_change.rb
@@ -1,3 +1,6 @@
+
+# These are migrated legacy records of changes made to (what were
+# called) Manuscript records.
 class EntryChange < ActiveRecord::Base
   belongs_to :entry
   belongs_to :changed_by, :class_name => 'User'
