7200f36ceec051d7077d94c1aa21adb946f02a1b
Benjamin Heller <echo29x@gmail.com>
2016-03-16 10:24:07 -0400

    searching/filtering on user activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,11 +5,41 @@ class ActivitiesController < ApplicationController
 
   def index
     page_size = 25
-    total = Activity.count
-    @num_pages = total / page_size
-    @num_pages += 1 if total % page_size > 0
     @page = (params["page"] || 1).to_i
-    @activities = Activity.order(id: :desc).limit(page_size).offset((@page - 1) * page_size)
+    @activities = []
+    last = 0
+    activity_list = Activity.joins(:user).where(params_for_index).order(id: :desc).limit(page_size).offset((@page - 1) * page_size)
+    @total = Activity.joins(:user).where(params_for_index).count
+
+    @num_pages = @total / page_size
+    @num_pages += 1 if @total % page_size > 0
+    activity_list.each do |activity|
+      if !@activities[last]
+        @activities[last] = [activity]
+      elsif @activities[last].first.user_id == activity.user_id && @activities[last].first.item_type == activity.item_type && @activities[last].first.event == activity.event
+        @activities[last].append(activity)
+      else
+        last += 1
+        @activities.append([activity])
+      end
+    end
+    respond_to do |format|
+      format.html {
+      }
+      format.json {
+        render json: @activities
+      }
+    end
+  end
+
+  def params_for_index
+    hash = {}
+    hash[:users] = params[:users] if (!params[:users].blank? && !params[:users][:username][0].blank?)
+    hash[:event] = params[:event] if !params[:event].blank?
+    hash[:item_type] = params[:item_type] if !params[:item_type].blank?
+    hash
+    # the (below) does not work, for some reason, so I did it manually (above)... thanks rails!
+    #params.permit(:event, :item_type, :users => [:username]).reject{|_, v| v.blank?}
   end
 
 end
