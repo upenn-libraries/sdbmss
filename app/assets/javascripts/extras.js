e1ee8ec161eed445d1fb5e73fdbbe3fe5a8b2f3e
Benjamin Heller <echo29x@gmail.com>
2016-04-07 14:59:25 -0400

    preliminary work on linking tables

--- app/assets/javascripts/extras.js
+++ app/assets/javascripts/extras.js
@@ -14,6 +14,7 @@
 //  require dataTables.colReorder
 //= require dataTables.colResize
 //= require dataTables.fixedColumns
+//= require doubleScroll
 //= require sdbmTable
 //= require sdbmManageRecords
 //= require sdbmApp
