b0386902654da01229855dd4c382dd49d0130238
Benjamin Heller <echo29x@gmail.com>
2016-10-18 09:56:18 -0400

    group update permission fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -66,7 +66,7 @@ class Ability
 
     can [:edit, :update], Entry, contributors: { :id => user.id }
     cannot :manage, [Group]
-    can :edit, Group, admin: { :id => user.id }
+    can [:edit, :update], Group, admin: { :id => user.id }
 =begin
     the old definitions - I am keeping them here now just for reference...
 
