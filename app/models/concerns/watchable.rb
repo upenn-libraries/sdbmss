a50fb2b76b8015f0a4f397d8e154dfbe0553abf1
Benjamin Heller <echo29x@gmail.com>
2017-01-11 15:19:35 -0500

    what

--- app/models/concerns/watchable.rb
+++ app/models/concerns/watchable.rb
@@ -3,7 +3,7 @@ module Watchable
   extend ActiveSupport::Concern
 
   included do
-    has_many :watches, as: :watched
+    has_many :watches, as: :watched, dependent: :destroy
     has_many :watchers, through: :watches, source: :user
   end
 
