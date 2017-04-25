00b41962b06dca154b7f8a8496d54159d159a83b
Benjamin Heller <echo29x@gmail.com>
2017-04-25 10:00:26 -0400

    partial legacy announcement handling

--- app/controllers/legacy_controller.rb
+++ app/controllers/legacy_controller.rb
@@ -3,10 +3,21 @@
 class LegacyController < ApplicationController
   def index
     entry_id = params[:id].to_s.gsub('SCHOENBERG_', '')
+    flash[:announce] = %q(
+      <div class='text-center'>
+        <span class='h3' style='color: #cc0000;'>Welcome to the New Schoenberg Database!</span><br>
+        <p class='text-left'>
+          The New Schoenberg Database of Manuscripts, in development since 2014, allows you -- the members of its user community -- 
+          to become active, contributing partners in its development. Users can add entries, comment on other users entries, aggregate 
+          entries to created “manuscript records,” and help us build an authority file of persons and institutions associated 
+          with the movement of manuscripts across time and place.
+        </p>
+        <a href="/pages/About">Learn more</a>
+      </div>
+    ).html_safe
     if Entry.exists?(id: entry_id)
       redirect_to entry_path(Entry.find(entry_id))
     else
-      flash[:notice] = "Record not found."
       redirect_to root_path
     end
   end
