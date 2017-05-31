c24ec922db1d1a954acaf1eb5dd129d4b88969d7
Benjamin Heller <echo29x@gmail.com>
2017-05-31 09:16:40 -0400

    bookmarks dericci fix (master)

--- app/models/dericci_record.rb
+++ app/models/dericci_record.rb
@@ -3,4 +3,29 @@ class DericciRecord < ActiveRecord::Base
   has_many :names, through: :dericci_links
 
   accepts_nested_attributes_for :dericci_links, allow_destroy: true
+
+  def public_id
+    "De Ricci #{id}"
+  end
+
+  def bookmark_details
+    results = {
+      name: name,
+      size: size,
+      url: url,
+      cards: cards,
+      place: place,
+      dates: dates
+    }
+    (results.select { |k, v| !v.blank? }).transform_keys{ |key| key.to_s.humanize }
+  end
+
+  def to_s
+    name
+  end
+
+  def preview
+    name
+  end
+
 end
