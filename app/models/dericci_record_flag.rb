61d41fbb6f798cbdea134eda74c04265bea55cef
Benjamin Heller <echo29x@gmail.com>
2017-06-14 11:14:50 -0400

    instructional content

--- app/models/dericci_record_flag.rb
+++ app/models/dericci_record_flag.rb
@@ -4,6 +4,6 @@ class DericciRecordFlag < ActiveRecord::Base
   include UserFields
 
   def self.reasons
-    ["It contains references to multiple possible names, and needs to be broken into several records", "The names or information contained are not relevant or applicable to names in the SDBM", "The name described here should be added to the SDBM Name Authority"]
+    ["The names or information contained are not relevant or applicable to names in the SDBM", "The name described here should be added to the SDBM Name Authority"]
   end
 end
