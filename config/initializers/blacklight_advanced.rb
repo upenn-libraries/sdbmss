1fea620f2029afeb2c020c559f6e20ef9fee25c3
Benjamin Heller <echo29x@gmail.com>
2017-08-29 12:58:01 -0400

    facet fix

--- config/initializers/blacklight_advanced.rb
+++ config/initializers/blacklight_advanced.rb
@@ -9,6 +9,34 @@
 # - a possible solution would be to figure out a way of loading ALL facets, then select the last X and continue normally
 # - memory issues for large facet list (authors has like 5000, I think?)
 
+module Blacklight::Catalog
+  def facet
+    @facet = blacklight_config.facet_fields[params[:id]]
+    if @facet
+      @response = get_facet_field_response(@facet.key, params)
+      @display_facet = @response.aggregations[@facet.key]
+
+      @pagination = facet_paginator(@facet, @display_facet)
+
+
+      respond_to do |format|
+        # Draw the facet selector for users who have javascript disabled:
+        format.html
+        format.json { render json: render_facet_list_as_json }
+
+        # Draw the partial for the "more" facet modal window:
+        format.js { render :layout => false }
+      end
+    else
+      respond_to do |format|
+        format.html { render "not_found" }
+        format.json { render json: {error: "Facet could not be found."} }
+        format.js { render json: {error: "Facet could not be found."} }
+      end
+    end
+  end
+end
+
 module Blacklight::Solr
   class FacetPaginator
     mattr_accessor :request_keys do
