de13eb128a69e5bbcce1f4665bf00f90cd4dc284
Benjamin Heller <echo29x@gmail.com>
2017-11-29 09:41:16 -0500

    removed dericci card warning

--- app/controllers/dericci_sales_controller.rb
+++ app/controllers/dericci_sales_controller.rb
@@ -1,7 +1,7 @@
 class DericciSalesController < ApplicationController
 
   def index
-    flash.now[:alert] = "<span class='lead'>Warning!</span> The server hosting the De Ricci Digitized Archive is not available.  Our records and workspace will not function correctly.".html_safe
+    #flash.now[:alert] = "<span class='lead'>Warning!</span> The server hosting the De Ricci Digitized Archive is not available.  Our records and workspace will not function correctly.".html_safe
     @sales = DericciSale.all
   end
 
