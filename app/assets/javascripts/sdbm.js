bcf8e138606c34ea08a2e950c8aa09c96444a1e9
Benjamin Heller <echo29x@gmail.com>
2017-11-30 11:14:28 -0500

    replaced bookmark uid with class, to handle awful jquery datatables behavior

--- app/assets/javascripts/sdbm.js
+++ app/assets/javascripts/sdbm.js
@@ -85,15 +85,21 @@ function bindRemoteAjaxCallback (){
   $('a[data-remote]').on('ajax:success', function (event, xhr, status, result) {
       //console.log('result.responseJSON', result.responseJSON);
       var errors = [];
-      for (var key in result.responseJSON.results) {
-        if (result.responseJSON.results[key].button_html) {
-          $("#" + key).replaceWith(result.responseJSON.results[key].button_html);          
-        } else if (result.responseJSON.results[key].error) {
-          errors.push(result.responseJSON.results[key].error);
-        }
+      console.log(result.responseJSON);
+      if (result.responseJSON.button) {
+        $(this).replaceWith(result.responseJSON.button);
       }
-      if (errors.length > 0) {
-        console.log("ERRORS: ", errors);        
+      else {
+        for (var key in result.responseJSON.results) {
+          if (result.responseJSON.results[key].button_html) {
+            $("." + key).html(result.responseJSON.results[key].button_html);          
+          } else if (result.responseJSON.results[key].error) {
+            errors.push(result.responseJSON.results[key].error);
+          }
+        }
+        if (errors.length > 0) {
+          console.log("ERRORS: ", errors);        
+        }
       }
       bindRemoteAjaxCallback();
   });
