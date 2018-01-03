18755f3252f54000fbc4fd8e91fb0497b8d62ba1
Benjamin Heller <echo29x@gmail.com>
2018-01-03 15:18:28 -0500

    visualization entirely switched over to chart.js

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -1,5 +1,4 @@
 // For pages (mostly post-login) with a lot of js functionality
 // This assumes application.js has been included.
 
-//= require d3
-//= require d3.geo
+//= require Chart.bundle.min
--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -15,7 +15,7 @@ class CommunityController < ApplicationController
           date = quantity.day
         end
         activity = Activity.includes(:user).where(event: "update").where("created_at > ?", Date.today - date).where.not(user: nil).group_by { |e| e.user }.map{ |key, value| [key.username, value.count, key.role]}
-        entries = Entry.where("created_at > ?", Date.today - date).where.not(created_by: nil).group_by { |e| e.created_by }.map{ |key, value| [key.username, value.count, key.role] }
+        entries = Entry.where("created_at > ?", Date.today - date).includes(:created_by).where.not(created_by: nil).group_by { |e| e.created_by }.map{ |key, value| [key.username, value.count, key.role] }
         render json: {result: "you did it!", entries: entries, activity: activity }
       }
     end
