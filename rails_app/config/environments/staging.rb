4c4362aba236ca7da5f5b743d23d87848adde2ab
andrewnk <andrewnk@upenn.edu>
2024-12-04 14:58:29 -0500

    Match staging env with prod

--- rails_app/config/environments/staging.rb
+++ rails_app/config/environments/staging.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -45,7 +45,6 @@ Rails.application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
-
   config.logger = Logger.new(STDOUT)
 
   # Prepend all log lines with the following tags.
