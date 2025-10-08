6003e924937d989378666a0aa6265fb722af4f1a
Patrick Perkins <pperkins@upenn.edu>
2025-10-08 14:46:34 -0400

    basic working capybara cuprite config with chrome in ansible

--- rails_app/app/assets/javascripts/application.js
+++ rails_app/app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require poltergeist_fix
 //= require jquery
 //= require thredded
 //
