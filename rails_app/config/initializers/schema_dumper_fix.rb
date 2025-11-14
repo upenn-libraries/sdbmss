3d216a53f24c0b2ba47d35fe4a8dc0d3731a9d51
Doug Emery <emeryr@upenn.edu>
2025-11-14 11:00:49 -0500

    Better, but not working frozen string fix

--- rails_app/config/initializers/schema_dumper_fix.rb
+++ rails_app/config/initializers/schema_dumper_fix.rb
@@ -1,40 +1,15 @@
-# frozen_string_literal: true
-# Patch column dumper for Rails 4.2 with Ruby 2.7
-# Addresses:
-#   Could not dump table "x" because of following FrozenError
-#   can't modify frozen String: "false"
-
-# Fix for FrozenError in schema.rb generation with Ruby 2.7+
-if Rails.version.start_with?("4.2")
-  require "active_record/schema_dumper"
-
-  module ActiveRecord
-    class SchemaDumper
-      private
-
-      def default_string(column)
-        return unless column.has_default?
-
-        # The issue is that column.default can be a frozen string
-        # and the original code tries to modify it in place
-        default = column.default
-
-        if default.is_a?(String)
-          default = default.dup # Create a mutable copy
-        end
-
-        case default
-        when BigDecimal
-          default.to_s
-        when String
-          default.inspect
-        when Date, DateTime, Time
-          "'#{default.to_s(:db)}'"
-        else
-          default.inspect
-        end
+# config/initializers/schema_dumper_thaw.rb
+ActiveSupport.on_load(:active_record) do
+  module ColumnDumperThaw
+    def prepare_column_options(column, types)
+      spec = super
+      if spec.key?(:default) && spec[:default].is_a?(String)
+        spec = spec.dup
+        spec[:default] = spec[:default].dup
       end
+      spec
     end
   end
-end
 
+  ActiveRecord::ConnectionAdapters::ColumnDumper.prepend ColumnDumperThaw
+end
