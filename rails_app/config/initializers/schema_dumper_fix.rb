52d1cdc3b9b36f216dd638c0534a608e69806183
Doug Emery <emeryr@upenn.edu>
2025-11-14 16:49:42 -0500

    Fix frozen string schema dump issue with Ruby 2.7/Rails 4.2

--- rails_app/config/initializers/schema_dumper_fix.rb
+++ rails_app/config/initializers/schema_dumper_fix.rb
@@ -1,15 +1,28 @@
-# config/initializers/schema_dumper_thaw.rb
-ActiveSupport.on_load(:active_record) do
-  module ColumnDumperThaw
-    def prepare_column_options(column, types)
-      spec = super
-      if spec.key?(:default) && spec[:default].is_a?(String)
-        spec = spec.dup
-        spec[:default] = spec[:default].dup
+# Address issues in dumped schemas like:
+#
+#  Could not dump table "comments" because of following FrozenError
+#    can't modify frozen String: "true"
+#
+# An issue with Ruby 2.7 and Rails 4.2; see: https://stackoverflow.com/a/68708411
+module ActiveRecord
+  module ConnectionAdapters
+    module ColumnDumper
+      def prepare_column_options(column, types)
+        spec = {}
+        spec[:name]      = column.name.inspect
+        spec[:type]      = column.type.to_s
+        spec[:null]      = 'false' unless column.null
+
+        limit = column.limit || types[column.type][:limit]
+        spec[:limit]     = limit.inspect if limit
+        spec[:precision] = column.precision.inspect if column.precision
+        spec[:scale]     = column.scale.inspect if column.scale
+
+        default = schema_default(column).dup if column.has_default?
+        spec[:default]   = default unless default.nil?
+
+        spec
       end
-      spec
     end
   end
-
-  ActiveRecord::ConnectionAdapters::ColumnDumper.prepend ColumnDumperThaw
 end
