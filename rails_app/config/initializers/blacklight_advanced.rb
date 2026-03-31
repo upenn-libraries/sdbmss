9004282df68ed0af555c85decae12332b30572cc
Kirk Wang <k3wang@gmail.com>
2026-03-30 23:41:16 -0700

    Update blacklight_advanced.rb initializer for BL6
    Remove BL5-only overrides that BL6 either absorbed or changed:
    - Blacklight::Solr::FacetPaginator: BL5 attempted asc/desc facet sorting
      via SOLR, but SOLR never supported it; BL6 dropped the concept
      entirely
    - Blacklight::Facet#facet_paginator: absorbed into BL6 internals
    - Blacklight::Solr::Response::Facets: BL6 rewrote
      facet_field_aggregations; the :order option and the key-aliasing loop
      are gone
    - Blacklight::Solr::SearchBuilderBehavior#add_facet_paging_to_solr:
      verbatim BL5 copy with no customization; removed, BL6 version used
    
    Remaining overrides updated for BL6:
    - Blacklight::Catalog#facet: wrap in after_initialize so module_eval
      runs after eager loading (plain module open shadows BL6's
      AS::Concern); fix aggregations lookup from @facet.key to @facet.field
    - Blacklight::SearchHelper#fetch_one: simplify now that solr_doc_params
      is gone; wrap in after_initialize for the same reason
    
    Refs:
    - https://github.com/projectblacklight/blacklight/commit/d75ff8594463f3c078ea0b0e51fc48ed15bef70e
    - https://github.com/projectblacklight/blacklight/commit/92ae811511da8eb3b00ed0dae1b8d84a31cd1300
    - https://github.com/projectblacklight/blacklight/commit/bd6224fead0fd9d2df9289b7800db7f8ad6afaec

--- rails_app/config/initializers/blacklight_advanced.rb
+++ rails_app/config/initializers/blacklight_advanced.rb
@@ -1,195 +1,55 @@
 
-# 
+#
 # various overrides for blacklight here
-# 
+#
 
 # partially successful attempt to allow blacklight facet sorting with asc/desc option
-# 
+#
 # of course, it doesn't work because SOLR itself doesn't support this
 # - a possible solution would be to figure out a way of loading ALL facets, then select the last X and continue normally
 # - memory issues for large facet list (authors has like 5000, I think?)
 
-module Blacklight::Catalog
-  def facet
-    @facet = blacklight_config.facet_fields[params[:id]]
-    if @facet
-      @response = get_facet_field_response(@facet.key, params)
-      @display_facet = @response.aggregations[@facet.key]
+Rails.application.config.after_initialize do
+  Blacklight::Catalog.module_eval do
+    def facet
+      @facet = blacklight_config.facet_fields[params[:id]]
+      if @facet
+        @response = get_facet_field_response(@facet.key, params)
+        @display_facet = @response.aggregations[@facet.field]
 
-      @pagination = facet_paginator(@facet, @display_facet)
+        @pagination = facet_paginator(@facet, @display_facet)
 
+        respond_to do |format|
+          # Draw the facet selector for users who have javascript disabled:
+          format.html
+          format.json { render json: render_facet_list_as_json }
 
-      respond_to do |format|
-        # Draw the facet selector for users who have javascript disabled:
-        format.html
-        format.json { render json: render_facet_list_as_json }
-
-        # Draw the partial for the "more" facet modal window:
-        format.js { render :layout => false }
+          # Draw the partial for the "more" facet modal window:
+          format.js { render :layout => false }
+        end
+      else
+        respond_to do |format|
+          format.html { render "not_found" }
+          format.json { render json: {error: "Facet could not be found."} }
+          format.js { render json: {error: "Facet could not be found."} }
+        end
       end
-    else
-      respond_to do |format|
-        format.html { render "not_found" }
-        format.json { render json: {error: "Facet could not be found."} }
-        format.js { render json: {error: "Facet could not be found."} }
-      end
     end
   end
 end
 
-module Blacklight::Solr
-  class FacetPaginator
-    mattr_accessor :request_keys do
-      { sort: :'facet.sort', page: :'facet.page', order: :'facet.order' }
-    end
-
-    if Rails.version < "4.1"
-      self.request_keys = { sort: :'facet.sort', page: :'facet.page', order: :'facet.order' }
-    end
-
-    attr_reader :offset, :limit, :sort, :order
-
-    def initialize(all_facet_values, arguments)
-      # to_s.to_i will conveniently default to 0 if nil
-      @offset = arguments[:offset].to_s.to_i 
-      @limit = arguments[:limit]
-      # count is solr's default
-      @sort = arguments[:sort] || "count"
-      @order = arguments[:order] || "desc"
-
-      #puts "dsagfadfgafdg >>>>>>>>>>>>>>>> #{all_facet_values.inspect}"
-
-      @all = @order == "desc" ? all_facet_values : all_facet_values.reverse
-    end
-  end
-end
-
-module Blacklight
-  module Facet
-    def facet_paginator(field_config, display_facet)
-      blacklight_config.facet_paginator_class.new(
-        display_facet.items,
-        sort: display_facet.sort,
-        offset: display_facet.offset,
-        limit: facet_limit_for(field_config.key),
-        order: display_facet.order
-      )
-    end
-  end
-end
-
-# because we are using blacklight and sunspot, I have to override this method since blacklight 
+# because we are using blacklight and sunspot, I have to override this method since blacklight
 # expects sunspot(solr) to index by ID as a number, but sunspot needs to index it by a string (Entry ID)
-# 
+#
 # to compensate, since it's literally a string change, I just do it here...
-
-module Blacklight::SearchHelper
-  def fetch_one(id, extra_controller_params)
-    old_solr_doc_params = Deprecation.silence(Blacklight::SearchHelper) do
-      solr_doc_params(id)
-    end
-
-    if default_solr_doc_params(id) != old_solr_doc_params
-      Deprecation.warn Blacklight::SearchHelper, "The #solr_doc_params method is deprecated. Instead, you should provide a custom SolrRepository implementation for the additional behavior you're offering. The current behavior will be removed in Blacklight 6.0"
-      extra_controller_params = extra_controller_params.merge(old_solr_doc_params)
-    end
-
-    # here!
-    id = "Entry #{id}"
-
-    solr_response = repository.find id, extra_controller_params
-    [solr_response, solr_response.documents.first]
-  end
-end
-
-module Blacklight::Solr
-  module SearchBuilderBehavior
-    def add_facet_paging_to_solr(solr_params)
-      return unless facet.present?
-
-      facet_config = blacklight_config.facet_fields[facet]
-
-      # Now override with our specific things for fetching facet values
-      facet_ex = facet_config.respond_to?(:ex) ? facet_config.ex : nil
-      solr_params[:"facet.field"] = with_ex_local_param(facet_ex, facet)
-
-      limit = if scope.respond_to?(:facet_list_limit)
-                scope.facet_list_limit.to_s.to_i
-              elsif solr_params["facet.limit"]
-                solr_params["facet.limit"].to_i
-              else
-                20
-              end
-
-      page = blacklight_params.fetch(request_keys[:page], 1).to_i
-      offset = (page - 1) * (limit)
-
-      sort = blacklight_params[request_keys[:sort]]
-
-      # Need to set as f.facet_field.facet.* to make sure we
-      # override any field-specific default in the solr request handler.
-      solr_params[:"f.#{facet}.facet.limit"] = limit + 1
-      solr_params[:"f.#{facet}.facet.offset"] = offset
-      if blacklight_params[request_keys[:sort]]
-        solr_params[:"f.#{facet}.facet.sort"] = sort
-      end
-
-      if blacklight_params[request_keys[:order]]
-        solr_params[:"f.#{facet}.facet.order"] = blacklight_params[request_keys[:order]]
-      end
-
-      solr_params[:rows] = 0
-    end  
-  end
-end
-
-module Blacklight::SolrResponse::Facets
-  class FacetField
-
-    def order
-      @options[:order]
+Rails.application.config.after_initialize do
+  Blacklight::SearchHelper.module_eval do
+    def fetch_one(id, extra_controller_params)
+      id = "Entry #{id}"
+      solr_response = repository.find id, extra_controller_params
+      [solr_response, solr_response.documents.first]
     end
   end
-
-  private
-
-  def facet_field_aggregations
-    list_as_hash(facet_fields).each_with_object({}) do |(facet_field_name, values), hash|
-      items = []
-      options = {}
-      values.each do |value, hits|
-        i = FacetItem.new(value: value, hits: hits)
-
-        # solr facet.missing serialization
-        if value.nil?
-          i.label = I18n.t(:"blacklight.search.fields.facet.missing.#{facet_field_name}", default: [:"blacklight.search.facets.missing"])
-          i.fq = "-#{facet_field_name}:[* TO *]"
-        end
-
-        items << i
-      end
-      options[:sort] = (params[:"f.#{facet_field_name}.facet.sort"] || params[:'facet.sort'])
-      if params[:"f.#{facet_field_name}.facet.limit"] || params[:"facet.limit"]
-        options[:limit] = (params[:"f.#{facet_field_name}.facet.limit"] || params[:"facet.limit"]).to_i
-      end
-
-      if params[:"f.#{facet_field_name}.facet.offset"] || params[:'facet.offset']
-        options[:offset] = (params[:"f.#{facet_field_name}.facet.offset"] || params[:'facet.offset']).to_i
-      end
-
-      options[:order] = (params[:"f.#{facet_field_name}.facet.order"] || params[:'facet.order'])
-
-      hash[facet_field_name] = FacetField.new(facet_field_name, items, options)
-
-      if blacklight_config and !blacklight_config.facet_fields[facet_field_name]
-        # alias all the possible blacklight config names..
-        blacklight_config.facet_fields.select { |k,v| v.field == facet_field_name }.each do |key,_|
-          hash[key] = hash[facet_field_name]
-        end
-      end
-    end
-  end
-
 end
 
 # Customize Blacklight and Blacklight Advanced Search parser to handle multiple inputs for the same field (i.e. author[]="Cicero"&author[]="Sallust" )
@@ -211,7 +71,7 @@ module BlacklightAdvancedSearch
           end
         else
           begin
-              queries << process_query_option(field, values, ParsingNesting::Tree.parse(query, config.advanced_search[:query_parser]).to_query( local_param_hash(field, config) ), options.shift)              
+            queries << process_query_option(field, values, ParsingNesting::Tree.parse(query, config.advanced_search[:query_parser]).to_query( local_param_hash(field, config) ), options.shift)
           rescue StandardError => e
             #
           end
@@ -240,7 +100,7 @@ module BlacklightAdvancedSearch
   end
 
   module RenderConstraintsOverride
-    
+
     # override default search constraints display, since we're moving facets elsewhere
     def render_constraints(localized_params = params)
       render_constraints_query(localized_params)
@@ -249,7 +109,7 @@ module BlacklightAdvancedSearch
     def query_has_constraints?(localized_params = params)
       if is_advanced_search? localized_params
         true
-      else    
+      else
         !(localized_params[:q].blank?)
       end
     end
@@ -262,7 +122,7 @@ module BlacklightAdvancedSearch
         content << render_filter_element_side(facet, values, localized_params)
       end
 
-      safe_join(content.flatten, "\n")    
+      safe_join(content.flatten, "\n")
     end
 
     def render_filter_element_side(facet, values, localized_params)
@@ -282,7 +142,7 @@ module BlacklightAdvancedSearch
           # remove link
           link_to(content_tag(:span, '', :class => "glyphicon glyphicon-remove") + content_tag(:span, '[remove]', :class => 'sr-only'), search_action_path(remove_facet_params(facet, val, localized_params)), :class=>"remove facet-count")
         end
-      
+
       end, "\n")
     end
 
@@ -315,7 +175,7 @@ module BlacklightAdvancedSearch
             safe_join(content.flatten, "\n")
           end
         else
-          safe_join(content.flatten, "\n")    
+          safe_join(content.flatten, "\n")
         end
       end
     end
