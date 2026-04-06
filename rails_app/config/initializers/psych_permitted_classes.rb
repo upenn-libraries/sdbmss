ee79e2fc10917fb09260c0872191802482acea0d
Kirk Wang <k3wang@gmail.com>
2026-04-06 13:19:10 -0700

    Clean up comments and initializers
    This commit will remove some comments make in an earlier commit now that
    we have a more stable build.  Also it will move the
    config.active_record.yaml_column_permitted_classes into application.rb.

--- rails_app/config/initializers/psych_permitted_classes.rb
+++
@@ -1,8 +0,0 @@
-# Psych 3.1+ restricts YAML deserialization to safe classes by default.
-# The ActiveRecord session store and Blacklight save Ruby objects (like
-# HashWithIndifferentAccess) into YAML-serialized session data.
-# This allows those classes to be deserialized.
-Rails.application.config.active_record.yaml_column_permitted_classes = [
-  ActiveSupport::HashWithIndifferentAccess,
-  Symbol
-]
