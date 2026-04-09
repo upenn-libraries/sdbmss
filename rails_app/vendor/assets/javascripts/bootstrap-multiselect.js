e5f1197d94dc6990d4aa9c43aea9f29a6b3d5620
Kirk Wang <k3wang@gmail.com>
2026-04-09 14:32:19 -0700

    Fix user menu templates/partials styling
    One of the changes is in the boostrap-multiselect.js which is vendored.
    I'm not sure of the history of this file and why it's vendored vs
    compiled.  We might want to see about updating it to a more current
    version because it looks like it's still on Boostrap 3.

--- rails_app/vendor/assets/javascripts/bootstrap-multiselect.js
+++ rails_app/vendor/assets/javascripts/bootstrap-multiselect.js
@@ -388,7 +388,7 @@
 
             },
             enableHTML: false,
-            buttonClass: 'btn btn-default',
+            buttonClass: 'btn btn-outline-secondary',
             inheritClass: false,
             buttonWidth: 'auto',
             buttonContainer: '<div class="btn-group" />',
@@ -423,10 +423,10 @@
             disabledText: '',
             delimiterText: ', ',
             templates: {
-                button: '<button type="button" class="multiselect dropdown-toggle" data-toggle="dropdown"><span class="multiselect-selected-text"></span> <b class="caret"></b></button>',
+                button: '<button type="button" class="multiselect dropdown-toggle" data-toggle="dropdown"><span class="multiselect-selected-text"></span></button>',
                 ul: '<ul class="multiselect-container dropdown-menu"></ul>',
-                filter: '<li class="multiselect-item multiselect-filter"><div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span><input class="form-control multiselect-search" type="text"></div></li>',
-                filterClearBtn: '<span class="input-group-btn"><button class="btn btn-default multiselect-clear-filter" type="button"><i class="glyphicon glyphicon-remove-circle"></i></button></span>',
+                filter: '<li class="multiselect-item multiselect-filter"><div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="fa fa-search"></i></span></div><input class="form-control multiselect-search" type="text"></div></li>',
+                filterClearBtn: '<div class="input-group-append"><button class="btn btn-outline-secondary multiselect-clear-filter" type="button"><i class="fa fa-times-circle"></i></button></div>',
                 li: '<li><a tabindex="0"><label></label></a></li>',
                 divider: '<li class="multiselect-item divider"></li>',
                 liGroup: '<li class="multiselect-item multiselect-group"><label></label></li>'
