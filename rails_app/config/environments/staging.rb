b2ff3f54383d466589286c898815df953f6f5c6d
Doug Emery <emeryr@upenn.edu>
2026-04-17 12:54:19 -0400

    Remove call to method removed from Rail 5.1/2 API (config.active_record.raise_in_transactional_callbacks)

--- rails_app/config/environments/staging.rb
+++ rails_app/config/environments/staging.rb
@@ -87,8 +87,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.active_record.raise_in_transactional_callbacks = true
 end
 
 Rails.application.config.middleware.use ExceptionNotification::Rack,
